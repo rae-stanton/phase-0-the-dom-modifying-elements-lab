76cf6ef265b8ea1ed1b3d882818147dd8f0b9482
Rae Stanton <raestanton93@gmail.com>
2023-07-18 12:12:44 -0700

    JS dom mod lab
    This lab uses JS to modify existing HTML elements and add new ones.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Luna is the champion';
+
+document.body.appendChild(newHeader);
+
+
+window.newHeader = newHeader;
